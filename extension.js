c175e606b73a5e50ca928d6916b3707f6a5c3e25
Jaromír <JaromirIhln@users.noreply.github.com>
2025-09-29 20:25:59 +0200

    Update parsování syntaxe pro Google Apps Script

--- extension.js
+++ extension.js
@@ -1,8 +1,43 @@
-function activate() {}
+const vscode = require('vscode');
 
-function deactivate() {}
+const tokenTypes = ['keyword', 'string', 'number', 'comment'];
+const legend = new vscode.SemanticTokensLegend(tokenTypes, []);
 
-module.exports = {
-  activate,
-  deactivate
+function getTokenType(token) {
+  if (/^(function|var|if|else|return|const|let|new|class|extends|super|this)$/.test(token)) return tokenTypes.indexOf('keyword');
+  if (/^".*"$|^'.*'$/.test(token)) return tokenTypes.indexOf('string');
+  if (/^\d+$/.test(token)) return tokenTypes.indexOf('number');
+  if (/^\/\/|^\/\*/.test(token)) return tokenTypes.indexOf('comment');
+  return -1;
+}
+
+const provider = {
+  provideDocumentSemanticTokens(document) {
+    const tokensBuilder = new vscode.SemanticTokensBuilder(legend);
+    const text = document.getText();
+    const regex = /\b(function|var|if|else|return|const|let|new|class|extends|super|this)\b|(".*?"|'.*?')|(\d+)|\/\/.*$|\/\*[\s\S]*?\*\//gm;
+    let match;
+    while ((match = regex.exec(text)) !== null) {
+      const token = match[0];
+      const start = match.index;
+      const length = token.length;
+      const typeIdx = getTokenType(token);
+      if (typeIdx >= 0) {
+        const pos = document.positionAt(start);
+        tokensBuilder.push(pos.line, pos.character, length, typeIdx, 0);
+      }
+    }
+    return tokensBuilder.build();
+  }
 };
+
+const selector = { language: 'gas', scheme: 'file' };
+vscode.languages.registerDocumentSemanticTokensProvider(selector, provider, legend);
+// function activate() {}
+
+// function deactivate() {}
+
+// module.exports = {
+//   activate,
+//   deactivate
+// };
